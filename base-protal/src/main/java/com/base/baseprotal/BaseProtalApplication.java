5a94744e3a903fc708cf58b6313d9ba765a3c187
shiweisen <shiweisen@bluemoon.com.cn>
2020-11-23 15:01:14 +0800

    [add] 添加sleuth链路跟踪，base-protal->base-admin

---
+++ base-protal/src/main/java/com/base/baseprotal/BaseProtalApplication.java
@@ -0,0 +1,13 @@
+package com.base.baseprotal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseProtalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseProtalApplication.class, args);
+    }
+
+}
---
+++ base-protal/src/test/java/com/base/baseprotal/BaseProtalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.base.baseprotal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseProtalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
